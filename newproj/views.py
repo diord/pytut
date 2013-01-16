e86f1274fae240e7e321af4cefad3f40598d66f8
Diord <motoenot@gmail.com>
2013-01-16 09:04:40 +0400

    day 3 first commit

---
+++ newproj/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+import datetime
+
+def hello(request):
+    return HttpResponse("hello!!!")
+
+def current_datetime(request):
+	now = datetime.datetime.now()	
+	html = "<html><body>It is now %s.</body></html>" % now 
+	return HttpResponse(html)
+
+def hours_ahead_old(request, offset):
+	try: 
+		offset = int(offset)
+	except VlalueError:
+		raise Http404()
+	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)	
+	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt) 
+	return HttpResponse(html)	
+
+def hours_ahead(request, offset):
+	try:
+		offset = int(offset)
+	except ValueError:
+		raise Http404()
+	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+	return render_to_response('hours_ahead.html', {'hours_offset': offset, 'next_time': dt})
+
+def about(request):
+	html = "<html><body>version %s.</body></html>" % 0.1 
+	return HttpResponse(html)
