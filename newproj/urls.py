5fc80340d4dd795f944e88ad12173075634ebc0d
Diord <motoenot@gmail.com>
2013-01-18 15:03:50 +0400

    search

--- newproj/urls.py
+++ newproj/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-from newproj.views import hello, current_datetime, hours_ahead, about, display_meta
+from newproj.views import hello, current_datetime, hours_ahead, about, display_meta, search
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
 	('^about/$', about),
 	(r'^time/plus/(\d{1,2})/$', hours_ahead),
     ('^meta/$', display_meta),
-    
+    (r'^search/$', search),
     # Examples:
     # url(r'^$', 'newproj.views.home', name='home'),
     # url(r'^newproj/', include('newproj.foo.urls')),
--- newproj/views.py
+++ newproj/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from books.models import Book
 import datetime
 
 def hello(request):
@@ -34,7 +35,21 @@ def about(request):
 def display_meta(request):
     values = request.META.items()
     values.sort()
-    html = []
-    for k, v in values:
-        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
-    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+    return render_to_response('list_meta.html', {'list_meta': values})
+    from django.shortcuts import render_to_response
+
+
+def search(request):
+    errors = []
+    if 'q' in request.GET:
+        q = request.GET['q']
+        if not q:
+            errors.append('Enter a search term.')
+        elif len(q) > 20:
+            errors.append('Please enter at most 20 characters.')
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response('search_results.html',
+                {'books': books, 'query': q})
+    return render_to_response('search_form.html',
+        {'errors': errors})
